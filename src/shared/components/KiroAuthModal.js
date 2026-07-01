0e9d326ac0cab1b8fc03166b6f18dc8d5575f036
Azaky <arfanzaky10@gmail.com>
2026-07-01 07:23:21 +0700

    fix(automation): don't auto-close Kiro bulk modal on job completion

--- src/shared/components/KiroAuthModal.js
+++ src/shared/components/KiroAuthModal.js
@@ -170,7 +170,6 @@ export default function KiroAuthModal({
         onClose={onClose}
         onSuccess={() => {
           onImportSuccess?.();
-          onClose?.();
         }}
       />
     );
